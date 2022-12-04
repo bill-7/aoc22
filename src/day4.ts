46adf1faf76df9c52ebbce7fd35575f3da4612e5
bill.cuthbert <bill.cuthbert@candide.com>
2022-12-04 21:25:37 +0000

    4.2

--- src/day4.ts
+++ src/day4.ts
@@ -1,8 +1,8 @@
-import { log, n, s, read, num } from "./utils"
+import { log, n, read, num } from "./utils"
 
 const d4 = read("day4")
 
-const completeRange = (s: s) => {
+const completeRange = (s: string) => {
   const [a, b] = s.split("-").map(num)
   const fill = (acc: n[], i: n, t: n): n[] => {
     if (i == t + 1) return acc
@@ -11,12 +11,26 @@ const completeRange = (s: s) => {
   return fill([], a, b)
 }
 
+const fullOverlap = ([a, b]: n[][]) => {
+  return a.every(x => b.includes(x)) || b.every(y => a.includes(y))
+}
+
+const anyOverlap = ([a, b]: n[][]) => {
+  return a.some(x => b.includes(x)) || b.some(y => a.includes(y))
+}
+
 const res4_1 = d4
   .split("\n")
   .map(x => x.split(","))
   .map(x => x.map(completeRange))
-  .filter(
-    ([a, b]) => a.every(x => b.includes(x)) || b.every(y => a.includes(y))
-  ).length
+  .filter(fullOverlap).length
+
+const res4_2 = d4
+  .split("\n")
+  .map(x => x.split(","))
+  .map(x => x.map(completeRange))
+  .filter(anyOverlap).length
 
 console.log(res4_1)
+
+console.log(res4_2)
