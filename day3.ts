3f71ecb83eda689c815633594b52a4e9c7172533
bill.cuthbert <bill.cuthbert@candide.com>
2022-12-04 16:26:00 +0000

    ugly part 2

--- day3.ts
+++ day3.ts
@@ -1,4 +1,4 @@
-import { read, sum } from "./utils"
+import { log, read, sum } from "./utils"
 
 const d3 = read("./day3.txt")
 
@@ -9,13 +9,17 @@ const twoHalves = (s: string): Stringers => [
   s.slice(s.length / 2),
 ]
 
-const findMatch = ([a, b]: Stringers) => {
-  return a.split("").find(c => b.includes(c))!
+const findMatch = ([head, ...tail]: string[]) => {
+  return head.split("").find(c => tail.every(t => t.includes(c)))!
 }
 
 const alphaScore = (c: string) => {
   const isUpper = c.toUpperCase() === c ? 26 : 0
   return "abcdefghijklmnopqrstuvwxyz".indexOf(c.toLowerCase()) + isUpper + 1
+}
+
+const triplets = (_: string, i: number, xs: string[]) => {
+  return (i + 1) % 3 == 0 ? [xs[i - 2], xs[i - 1], _] : []
 }
 
 const res3_1 = d3
@@ -25,4 +29,14 @@ const res3_1 = d3
   .map(alphaScore)
   .reduce(sum)
 
+const res3_2 = d3
+  .split("\n")
+  .map(triplets)
+  .filter(x => x.length)
+  .map(log)
+  .map(findMatch)
+  .map(alphaScore)
+  .reduce(sum)
+
 console.log(res3_1)
+console.log(res3_2)
